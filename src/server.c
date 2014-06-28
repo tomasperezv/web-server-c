a588601b1d691cd5b9c021d7c10b895c831b958e
Tomas Perez <tom@0x101.com>
2014-06-28 21:34:14 +0200

    Server listening and sending a test message.

--- src/server.c
+++ src/server.c
@@ -53,7 +53,36 @@ char** read_config(char *filename, char **configuration) {
  * @method main
  */
 int main(int argc, char *argv[]) {
+
   char *configuration[255];
   read_config(FILENAME, configuration);
-  printf("Hello world %s", configuration[0]);
+
+  int listenfd = 0, connfd = 0;
+  struct sockaddr_in serv_addr;
+
+  char sendBuff[1025];
+  time_t ticks;
+
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  memset(sendBuff, '0', sizeof(sendBuff));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  serv_addr.sin_port = htons(atoi(configuration[0]));
+
+  bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+  listen(listenfd, 10);
+
+  while(1) {
+    connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+    ticks = time(NULL);
+    snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+    write(connfd, sendBuff, strlen(sendBuff));
+
+    close(connfd);
+    sleep(1);
+  }
+
 }
