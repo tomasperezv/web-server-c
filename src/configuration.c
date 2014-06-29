cb5d5f4c3a8c4cb1c8de17f8376f7f4b9be67630
Tomas Perez <tom@0x101.com>
2014-06-29 12:18:56 +0200

    Removing not required dependencies.

--- src/configuration.c
+++ src/configuration.c
@@ -6,16 +6,9 @@
  */
 
 #include "base.h"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
-#include <sys/types.h>
-#include <time.h>
 
 /*
  * @method read_config
