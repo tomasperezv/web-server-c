06bb0d1f61515594509c1bff0372745c9dd2a9e2
Tomas Perez <tom@0x101.com>
2014-09-07 20:35:31 +0200

    Adding new HTTP1.1 headers.

--- src/server.h
+++ src/server.h
@@ -1,5 +1,7 @@
-char connection_headers[3][256] = {
-  {"GET / HTTP/1.1\r\n"},
+char connection_headers[5][256] = {
+  {"GET / HTTP/1.1 200 OK\r\n"},
+  {"Transfer-Encoding: chunked\r\n"},
+  {"Server: https://github.com/tomasperezv/web-server-c\r\n"},
   {"Connection:close\r\n"},
-  {"Content-Type:text/html\r\n"}
+  {"Content-Type:text/html; charset=UTF-8\r\n"}
 };
